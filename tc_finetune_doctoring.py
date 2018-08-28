1fd4ab13bd8f2809325428e7f255919441332ccc
abby <sw62190>
2018-08-28 10:41:17 -0500

    train_data --> self; correct train file in finetune files

--- tc_finetune_doctoring.py
+++ tc_finetune_doctoring.py
@@ -36,7 +36,7 @@ def main(margin,batch_size,output_size,learning_rate,whichGPU,is_finetuning,pret
 
     ckpt_dir = './output/doctoring/ckpts'
     log_dir = './output/doctoring/logs'
-    train_filename = './input/train.txt'
+    train_filename = './input/train_by_hotel.txt'
     mean_file = './input/meanIm.npy'
 
     img_size = [256, 256]
--- tc_finetune_no_doctoring.py
+++ tc_finetune_no_doctoring.py
@@ -36,7 +36,7 @@ def main(margin,batch_size,output_size,learning_rate,whichGPU,is_finetuning,pret
 
     ckpt_dir = './output/no_doctoring/ckpts'
     log_dir = './output/no_doctoring/logs'
-    train_filename = './input/train.txt'
+    train_filename = './input/train_by_hotel.txt'
     mean_file = './input/meanIm.npy'
 
     img_size = [256, 256]
