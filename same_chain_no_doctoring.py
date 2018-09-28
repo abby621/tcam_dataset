bb5583a2dd4e4467d35b2c0296b5448a1d4c179e
abby <sw62190>
2018-09-28 13:43:14 -0500

    no people masks in no_doctoring

--- same_chain_no_doctoring.py
+++ same_chain_no_doctoring.py
@@ -173,10 +173,9 @@ def main(same_chain_margin,diff_chain_margin,batch_size,output_size,learning_rat
     for step in range(num_iters):
         start_time = time.time()
         batch, labels, chains, ims = train_data.getBatch()
-        people_masks = train_data.getPeopleMasks()
         batch_time = time.time() - start_time
         start_time = time.time()
-        _, loss_val = sess.run([train_op, loss], feed_dict={image_batch: batch, people_mask_batch: people_masks})
+        _, loss_val = sess.run([train_op, loss], feed_dict={image_batch: batch})
         end_time = time.time()
         duration = end_time-start_time
         out_str = 'Step %d: loss = %.6f -- (batch creation: %.3f | training: %.3f sec)' % (step, loss_val,batch_time,duration)
