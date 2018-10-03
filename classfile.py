89b4019f5dc6f680864ff69903770ec9ed84a344
abby <sw62190>
2018-10-03 13:26:02 -0500

    npairs batches

--- classfile.py
+++ classfile.py
@@ -387,3 +387,113 @@ class SameChainSet(CombinatorialTripletSet):
 
         imgs = np.array([imgs[ix,top[ix]:(top[ix]+self.crop_size[0]),left[ix]:(left[ix]+self.crop_size[1]),:]-self.meanImage for ix in range(numIms)])
         return imgs
+
+class SameChainNpairs(SameChainSet):
+    def __init__(self, image_list, class_to_chain_mapping, mean_file, image_size, crop_size, batchSize=100, isTraining=True, isOverfitting=False,fractionSameChain=0.5,randomizeChainFraction=False):
+        self.image_size = image_size
+        self.crop_size = crop_size
+        self.isTraining = isTraining
+        self.isOverfitting = isOverfitting
+
+        self.meanFile = mean_file
+        meanIm = np.load(self.meanFile)
+
+        if meanIm.shape[0] == 3:
+            meanIm = np.moveaxis(meanIm, 0, -1)
+
+        self.meanImage = cv2.resize(meanIm, (self.crop_size[0], self.crop_size[1]))
+
+        #img = img - self.meanImage
+        if len(self.meanImage.shape) < 3:
+            self.meanImage = np.asarray(np.dstack((self.meanImage, self.meanImage, self.meanImage)))
+
+        self.batchSize = batchSize
+
+        self.fractionSameChain = fractionSameChain
+        self.randomizeChainFraction = randomizeChainFraction
+
+        # this is SUPER hacky -- if the test file is 'occluded' then the class is in the 5th position, not the 4th
+        if 'occluded' in image_list:
+            clsPos = 4
+        else:
+            clsPos = 3
+
+        self.chains = {}
+        # Reads a .txt file containing image paths of image sets where each line contains
+        # all images from the same set and the first image is the anchor
+        f = open(image_list, 'r')
+        for line in f:
+            temp = line.strip('\n').split(' ')
+            hotel = int(temp[0].split('/')[clsPos])
+            if hotel in class_to_chain_mapping.keys():
+                chain = class_to_chain_mapping[hotel]
+            else:
+                chain = -1
+            if not chain in self.chains.keys():
+                 self.chains[chain] = {}
+            if not hotel in self.chains[chain].keys():
+                self.chains[chain][hotel] = {}
+                self.chains[chain][hotel]['ims'] = []
+            for t in temp:
+                if t not in self.chains[chain][hotel]['ims']:
+                    self.chains[chain][hotel]['ims'].append(t)
+            if len(self.chains[chain][hotel]['ims']) < 2:
+                self.chains[chain].pop(hotel)
+            else:
+                self.chains[chain][hotel]['sources'] = np.array([im.split('/')[clsPos+1] for im in self.chains[chain][hotel]['ims']])
+
+        self.people_crop_files = glob.glob(os.path.join(peopleDir,'*.png'))
+
+    def getBatch(self):
+        numClasses = self.batchSize/2
+        chain = np.random.choice(self.chains.keys())
+
+        classes = np.zeros(numClasses,dtype='int')
+        chains = np.zeros(numClasses,dtype='int')
+
+        # the first fraction of the classes in the batch should be from the same chain -- this is a version of hard mining,
+        # making it so half of the negative examples we see are "harder" because they come from the same chain
+        while len(self.chains[chain].keys()) < int(float(numClasses)*self.fractionSameChain):
+            chain = np.random.choice(self.chains.keys())
+
+        classes[:int(float(numClasses)*self.fractionSameChain)] = np.random.choice(self.chains[chain].keys(),int(float(numClasses)*self.fractionSameChain),replace=False)
+        chains[:int(float(numClasses)*self.fractionSameChain)] = chain
+
+        # the other fraction of the classes should be from random hotels
+        for iy in range(int(float(numClasses)*self.fractionSameChain),numClasses):
+            chain2 = np.random.choice(self.chains.keys())
+            numHotelsAvailable = np.sum(np.array([1 for h in self.chains[chain2].keys() if h not in classes]))
+            while chain2 == chain or numHotelsAvailable < 1:
+                chain2 = np.random.choice(self.chains.keys())
+                numHotelsAvailable = np.sum(np.array([1 for h in self.chains[chain2].keys() if h not in classes]))
+            hotel = np.random.choice(self.chains[chain2].keys())
+            while hotel in classes:
+                hotel = np.random.choice(self.chains[chain2].keys())
+            classes[iy] = hotel
+            chains[iy] = chain2
+
+        ims = []
+        labels = [c for c in classes for ix in range(self.numPos)]
+        for hotel,chain in zip(classes,chains):
+            clsPaths = np.array(self.chains[chain][hotel]['ims'])
+            clsSources = np.array(self.chains[chain][hotel]['sources'])
+            tcamInds = np.where(clsSources=='tcam')[0]
+            exInds = np.where(clsSources=='expedia')[0]
+            if len(tcamInds) >= 1 and len(exInds) >= 1:
+                numTcam = 1
+                numEx = 1
+            elif len(tcamInds) > 1 and len(exInds) < 1:
+                numEx = 2
+                numTcam = 0
+            else:
+                numTcam = 2
+                numEx = 0
+
+            random.shuffle(tcamInds)
+            random.shuffle(exInds)
+            ims.extend(list(clsPaths[tcamInds[:numTcam]]))
+            ims.extend(list(clsPaths[exInds[:numEx]]))
+
+        batch = self.getProcessedImages(ims)
+        chains = [c for c in chains for ix in range(2)]
+        return batch, labels, chains, ims
