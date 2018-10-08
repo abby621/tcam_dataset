444a9e6222e6ad72b963961bf0b4d49a79093ea7
abby <sw62190>
2018-10-07 21:40:19 -0500

    fixing issue with data augmentation

--- npairs_doctoring.py
+++ npairs_doctoring.py
@@ -262,7 +262,7 @@ def main(batch_size,output_size,learning_rate,whichGPU,is_finetuning,is_overfitt
         people_masks = train_data.getPeopleMasks()
         batch_time = time.time() - start_time
         start_time = time.time()
-        _, fb, loss_val = sess.run([train_op, final_batch, loss], feed_dict={image_batch: batch,label_batch:hotels, people_mask_batch: people_masks})
+        _, fb, loss_val = sess.run([train_op, masked_batch, loss], feed_dict={image_batch_mean_subtracted: batch,label_batch:hotels, people_mask_batch: people_masks})
         end_time = time.time()
         duration = end_time-start_time
         out_str = 'Step %d: loss = %.6f (batch creation: %.3f | training: %.3f sec)' % (step, loss_val, batch_time,duration)
